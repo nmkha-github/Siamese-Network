640c7c1761603b4c869dafd6e49dcb4834745e2e
nmkha-gitlab <nguyenminhkha2001@gmail.com>
2023-09-12 00:36:08 +0700

    fix bug load

--- SiameseNetwork.py
+++ SiameseNetwork.py
@@ -115,6 +115,6 @@ class SiameseNetwork(nn.Module):
         # Move the model to the appropriate device (cuda or cpu)
         self.to(state_dict['fc.0.weight'].device)
         
-        print('current epoch: ' + self.epochs)
+        print('current epoch: ' + str(self.epochs))
 
         
