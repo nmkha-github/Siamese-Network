7828d6b2834b2b809db207d78b71ed05831a0811
nmkha-gitlab <nguyenminhkha2001@gmail.com>
2023-09-12 00:03:57 +0700

    remove print

--- ContrastiveLoss.py
+++ ContrastiveLoss.py
@@ -11,7 +11,6 @@ class ContrastiveLoss(torch.nn.Module):
         self.margin = margin
 
     def forward(self, output0, output1, label):
-        print(label)
         euclidean_distance = F.pairwise_distance(output0, output1, keepdim=True)
         loss_contrastive = torch.mean((1-label) * torch.pow(euclidean_distance, 2) +
                                       (label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
