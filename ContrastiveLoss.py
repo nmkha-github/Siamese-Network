ed3ccc65597395cd1155cf657d2954e2ada86d33
nmkha-gitlab <nguyenminhkha2001@gmail.com>
2023-09-11 23:55:10 +0700

    update loss function

--- ContrastiveLoss.py
+++ ContrastiveLoss.py
@@ -1,23 +1,18 @@
 import torch
-
+import torch.nn.functional as F
 
 class ContrastiveLoss(torch.nn.Module):
     """
     Contrastive loss function.
     """
 
-    def __init__(self, margin=1.0):
+    def __init__(self, margin=2.0):
         super(ContrastiveLoss, self).__init__()
         self.margin = margin
 
-    def forward(self, x0, x1, y):
-        # euclidian distance
-        diff = x0 - x1
-        dist_sq = torch.sum(torch.pow(diff, 2), 1)
-        dist = torch.sqrt(dist_sq)
-
-        mdist = self.margin - dist
-        dist = torch.clamp(mdist, min=0.0)
-        loss = y * dist_sq + (1 - y) * torch.pow(dist, 2)
-        loss = torch.sum(loss) / 2.0 / x0.size()[0]
-        return loss
+    def forward(self, output0, output1, label):
+        print(label)
+        euclidean_distance = F.pairwise_distance((output0, output1), keepdim=True)
+        loss_contrastive = torch.mean((1-label) * torch.pow(euclidean_distance, 2) +
+                                      (label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
+        return loss_contrastive
--- SiameseNetwork.py
+++ SiameseNetwork.py
@@ -98,8 +98,11 @@ class SiameseNetwork(nn.Module):
             
         return self.loss_values
 
-    def save(self):
-        torch.save(self.state_dict(), "saved_models/Siamese_epoch_{}.pt".format(self.epochs))
+    def save(self, path=''):
+        if path != '':
+            torch.save(self.state_dict(), path + "/Siamese_epoch_{}.pt".format(self.epochs))
+        else:
+            torch.save(self.state_dict(), "saved_models/Siamese_epoch_{}.pt".format(self.epochs))
         print("Model Saved Successfully") 
         
     def load(self, path):
